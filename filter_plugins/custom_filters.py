bc90542368eb5640a8767fbf01b8590585bc4c99
mabq <alejandro.banderas@me.com>
2023-11-13 18:44:17 -0500

    all

---
+++ filter_plugins/custom_filters.py
@@ -0,0 +1,9 @@
+def remove_hash_from_hex_color(hex_color):
+    if hex_color.startswith("#"):
+        return hex_color[1:]
+    else:
+        return hex_color
+
+class FilterModule(object):
+    def filters(self):
+        return {'remove_hash_from_hex_color': remove_hash_from_hex_color}
