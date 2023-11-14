0030d7cb499a0bb9f37c9737e7d07b5ee57348bd
mabq <alejandro.banderas@me.com>
2023-11-13 19:31:11 -0500

    all

--- roles/base/templates/zsh/colors-tty.sh
+++ roles/base/templates/zsh/colors-tty.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# P0 must is low because it is used for background
+# P7 is high because it is used for text
+# p8 is highlight because `color_black_high` does not have enough contrast
+
 [ "${TERM:-none}" = "linux" ] && \
     printf '%b' '\e]P0{{ color_black_low | remove_hash_from_hex_color }}
                  \e]P1{{ color_red | remove_hash_from_hex_color }}
@@ -9,7 +13,7 @@
                  \e]P5{{ color_magenta | remove_hash_from_hex_color }}
                  \e]P6{{ color_cyan | remove_hash_from_hex_color }}
                  \e]P7{{ color_white_high | remove_hash_from_hex_color }}
-                 \e]P8{{ color_black_high | remove_hash_from_hex_color }}
+                 \e]P8{{ color_highlight_high | remove_hash_from_hex_color }}
                  \e]P9{{ color_red_high | remove_hash_from_hex_color }}
                  \e]PA{{ color_green_high | remove_hash_from_hex_color }}
                  \e]PB{{ color_yellow_high | remove_hash_from_hex_color }}
