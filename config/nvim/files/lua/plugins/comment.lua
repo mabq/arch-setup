cc0dd2d030ceb183f8e11ad9f0ec517e49e279d6
mabq <alejandro.banderas@me.com>
2024-05-30 23:06:56 -0500

    all

--- config/nvim/files/lua/plugins/comment.lua
+++ config/nvim/files/lua/plugins/comment.lua
@@ -1,7 +1,30 @@
 return {
 	{
-		enabled = false,
+		enabled = true,
+		"echasnovski/mini.comment",
+		version = false,
+		config = function()
+			require("mini.comment").setup({
+				options = {
+					custom_commentstring = function()
+						return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+					end,
+				},
+			})
+		end,
+	},
+	{
+		-- Add support for JSX comments and other embedded languages (requires tree-sitter and mini.comment)
+		enabled = true,
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	{
+		enabled = false, -- replaced with mini.comment
 		"numToStr/Comment.nvim",
-		opts = {}, -- force loading
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
 	},
 }
