15d4fbc1a91c5fca48f9dc30f0ad40556ecfd60c
mabq <alejandro.banderas@me.com>
2024-05-31 13:43:20 -0500

    all

--- config/nvim/files/lua/plugins/mini-comment.lua
+++ config/nvim/files/lua/plugins/mini-comment.lua
@@ -18,13 +18,13 @@ return {
 		enabled = true,
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-	{
-		enabled = false, -- replaced with mini.comment
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-	},
+	-- {
+	-- 	enabled = false, -- replaced with mini.comment
+	-- 	"numToStr/Comment.nvim",
+	-- 	config = function()
+	-- 		require("Comment").setup({
+	-- 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	-- 		})
+	-- 	end,
+	-- },
 }
