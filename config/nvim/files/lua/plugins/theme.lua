7165ebfb14a5f0512bd0074484d32bbe95afcdbc
mabq <alejandro.banderas@me.com>
2024-08-04 01:19:01 -0500

    all

--- config/nvim/files/lua/plugins/theme.lua
+++ config/nvim/files/lua/plugins/theme.lua
@@ -6,6 +6,12 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			require("tokyonight").setup({
+				style = "moon",
+				on_highlights = function(hl, c)
+					hl.CursorLine.bg = "#1d1e2a"
+				end,
+			})
 			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
