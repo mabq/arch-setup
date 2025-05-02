5c23e6601a4799095cf7b97d91f70e0de2c4547e
mabq <alejandro.banderas@me.com>
2025-05-02 17:30:58 -0500

    Hyprpicker - solve toggle not closing

--- config/hypr/files/scripts/pick-color.sh
+++ config/hypr/files/scripts/pick-color.sh
@@ -11,6 +11,14 @@
 # hyprland keybindings
 #
 
+
+# Check if hyprpicker is running (the `> /dev/null` part is important!)
+if pgrep --exact "hyprpicker" > /dev/null; then
+    # Kill hyprpicker if running
+    pkill --exact "hyprpicker"
+    return 0
+fi
+
 # Use the color format passed as argument (hyprland keybinds) or hex if none is passed
 if [ $# -eq 1 ]; then
     format=$1
